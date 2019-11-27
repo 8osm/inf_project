4d436e3eeaa04de46675ef4d22d3fa8f27a5c10d
8osm <zareichuk@yandex.ua>
2019-11-27 21:01:58 +0200

    yeah boiiii

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+    app = express(),
+    morgan = require('morgan');
+
+app.use(morgan('dev'));
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(5858, () => console.log('Listening on 5858!'));
